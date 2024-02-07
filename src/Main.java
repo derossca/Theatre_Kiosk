eaf4a0744b7cfcceac5cc81a41cf82187f937b13
Corey DeRossett <cderossett3@gmail.com>
2024-02-07 17:53:09 -0500

    imported scanner, added psuedo code, declared variables, outputed asking for user age

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+//importing scanner
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //psuedo code
+        //class WristBand
+        //	main()
+        //		num age = 0
+        //		num DRINKING_AGE = 21
+        //		output “Enter your age:’
+        //		input age
+        //		if age >= DRINKING_AGE then
+        //			output “You get a wrist band. Drink responsibly!”
+        //		end if
+        //	return
+        //end class
+
+        //declaring variables
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+        int DRINKING_AGE = 21;
+        //output asking for user age
+        System.out.println();
     }
 }
